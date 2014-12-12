125526c2b4939304d2561a5aa6d571c8cd24f000
tom <tom@antivapor.net>
2014-12-12 10:36:09 -0800

    update requirements for travis build pass

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def pypi_rst_long(readme):
     try:
         import pypandoc
         desc = pypandoc.convert(readme,'rst')
-    except:
+    except (IOError, ImportError):
         pass
     return desc
 
@@ -17,16 +17,25 @@ setup(name='vondrak',
       description = "a Python implementation of Vondrák's long term precession model",
       long_description = pypi_rst_long('readme.md'),
       url = 'https://github.com/digitalvapor/vondrak',
-      download_url = 'https://github.com/digitalvapor/vondrak/tarball/{}'.format(__version__),
+      download_url = 'https://github.com/digitalvapor/vondrak/tarball/{0}'.format(__version__),
       author = 'Tom Spalding',
       author_email = 'tommycs@mail.sfsu.edu',
       keywords = ['astronomy', 'precession', 'vondrak', 'space', 'proper motion'],
       license = 'Creative Commons Attribution-ShareAlike 4.0 International License',
-      packages = ['vondrak'],
+      packages = [
+        'vondrak',
+        'vondrak.tests',
+        ],
+      install_requires = ['numpy'],
       classifiers = [
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Astronomy',
       ],
       )
