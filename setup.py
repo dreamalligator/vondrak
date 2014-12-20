5f73f4adddfb61aa567bf18b8fbad6da09eba037
tom <tom@antivapor.net>
2014-12-20 10:15:47 -0800

    update setup

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+import os
+from setuptools import setup
 from vondrak import __version__
 
-setup(name='vondrak',
-      version = __version__,
-      description = "a Python implementation of Vondrák's long term precession model and Fortran code",
-      long_description = open('readme.rst').read(),
-      url = 'https://github.com/digitalvapor/vondrak',
-      download_url = 'https://github.com/digitalvapor/vondrak/tarball/{0}'.format(__version__),
-      author = 'Tom Spalding',
-      author_email = 'tommycs@mail.sfsu.edu',
-      keywords = ['astronomy', 'precession', 'vondrak', 'space', 'proper motion'],
-      license = 'Creative Commons Attribution-ShareAlike 4.0 International License',
-      packages = [
+def read(*filenames):
+    return open(os.path.join(os.path.dirname(__file__), *filenames)).read()
+
+setup(
+    name='vondrak',
+    version = __version__,
+    description = "A Python implementation of Vondrák's long term precession model and Fortran code.",
+    long_description = read('readme.rst'),
+    url = 'https://github.com/digitalvapor/vondrak',
+    download_url = 'https://github.com/digitalvapor/vondrak/tarball/{0}'.format(__version__),
+    author = 'Tom Spalding',
+    author_email = 'tommycs@mail.sfsu.edu',
+    keywords = ['astronomy', 'precession', 'vondrak', 'space', 'proper motion'],
+    license = 'Creative Commons Attribution-ShareAlike 4.0 International License',
+    packages = [
         'vondrak',
         'vondrak.tests',
-        ],
-      install_requires = ['numpy'],
-      classifiers = [
+    ],
+    package_data = {
+        'vondrak': ['docs/*.rst'],
+    },
+    install_requires = ['numpy'],
+    classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
@@ -27,5 +35,4 @@ setup(name='vondrak',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Astronomy',
-      ],
-      )
+    ])
